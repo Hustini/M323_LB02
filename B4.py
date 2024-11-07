e02a2e96ae8f7d3c30c98470ca0596cc3184227e
Hustini <ejdersalih02@gmail.com>
2024-11-07 15:53:26 +0100

    added endpoints

--- B4.py
+++ B4.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -5,34 +6,43 @@ app = Flask(__name__)
 
 @app.route('/B4G', methods=['GET'])
 def funktion():
-    square = lambda x: x * x
-    result = square(5)
+    numbers = [1, 2, 3, 4, 5]
+    squared_numbers = list(map(lambda x: x ** 2, numbers))
+    even_numbers = list(filter(lambda x: x % 2 == 0, numbers))
+    sum_of_numbers = reduce(lambda x, y: x + y, numbers)
 
     return jsonify({
-        'approach': 'simple lambda func',
-        'result': result
+        'approach': 'map, filter, reduce',
+        'result': f'map: {squared_numbers}, filter: {even_numbers}, reduce: {sum_of_numbers}',
     })
 
 
 @app.route('/B4F', methods=['GET'])
 def funktion_2():
-    add = lambda x, y: x + y
-    result = add(5, 3)
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    result = map(lambda x: x * 2, filter(lambda x: x % 2 == 0, numbers))
 
     return jsonify({
-        'approach': 'complex lambda func',
-        'result': result,
+        'approach': 'list of all even numbers double',
+        'result': list(result),
     })
 
 
 @app.route('/B4E', methods=['GET'])
 def funktion_3():
-    words = ["apple", "banana", "kiwi", "pear", "grapefruit"]
-    result = sorted(words, key=lambda word: len(word))
+    cart = [
+        {"product": "A", "price": 15, "quantity": 2},
+        {"product": "B", "price": 25, "quantity": 1},
+        {"product": "C", "price": 30, "quantity": 3},
+        {"product": "D", "price": 20, "quantity": 1}
+    ]
+
+    expensive_products = filter(lambda item: item["price"] > 20, cart)
+    result = map(lambda item: item["price"] * item["quantity"], expensive_products)
 
     return jsonify({
-        'approach': 'sorted with lambda func',
-        'result': f'{result}'
+        'approach': 'Total value per product where price over 20',
+        'result': list(result)
     })
 
 
